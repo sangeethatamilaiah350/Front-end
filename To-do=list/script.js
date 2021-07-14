02ca40c23469615e561c38ac4407e8fc9e421bfa
SANGEETHA <66882971+sangeethatamilaiah350@users.noreply.github.com>
2021-07-15 00:04:08 +0530

    Update script.js

--- To-do=list/script.js
+++ To-do=list/script.js
@@ -1,24 +1,30 @@
 let input=document.getElementById("input");
 let adding=document.getElementById("add");
 let display=document.getElementById("inside");
-
+let value
 adding.addEventListener("click",function(){
     var lists=document.createElement('p');
     var l=document.createElement('hr');
     l.classList.add("line")
     lists.classList.add("paragraph-styling");
+    value=input.value;
     lists.innerText=input.value;
     display.appendChild(lists);
     display.appendChild(l)
     input.value="";
+    hitt("http://127.0.0.1:5000/"+value)
     lists.addEventListener("click",function(){
         lists.style.textDecoration="line-through";
     });
     lists.addEventListener("dblclick",function(){
         display.removeChild(lists);
         display.removeChild(l)
+        hitt("http://127.0.0.1:5000/del/"+value)
     });
 });
-
+async function hitt(a){
+    const response=await fetch(a)
+    var data=await response.json()
+}
 
 
